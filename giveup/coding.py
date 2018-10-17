6eb6511fe3de19fc61a1b4ca7944dced286f3d64
yuqihui <mg5851@126.com>
2018-10-17 17:55:13 +0800

    hh

---
+++ giveup/coding.py
@@ -0,0 +1,16 @@
+# from restaurant import Restraurant
+#
+# restraurant1 = Restraurant('A','B')
+# print(restraurant1.restaurant_name," ",restraurant1.cuisine_type)
+# restraurant1.describe_restraurant()
+# restraurant1.open_restaurant()
+# print(restraurant1.get_number_served())
+# restraurant1.set_number_served(2)
+# print(restraurant1.get_number_served())
+# restraurant1.increment_number_served(10)
+
+
+
+from user import Admin
+admin = Admin("Alice",'Bob')
+admin.show_privileges()
