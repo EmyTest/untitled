e26aed15364cdcb95b164576a91f098920599fd4
yuqihui <mg5851@126.com>
2018-10-17 14:17:50 +0800

    hh

--- giveup/p1016.py
+++ giveup/p1016.py
@@ -41,9 +41,73 @@
 
 
 
+# class Restraurant():
+#     def __init__(self,restraurant_name,cuisine_type):
+#         self.restaurant_name = restraurant_name
+#         self.cuisine_type = cuisine_type
+#         self.number_served = 0
+#
+#     def describe_restaurant(self):
+#         print(self.restaurant_name,":",self.cuisine_type)
+#
+#     def open_restaurant(self):
+#         print("is opening")
+#
+#     def set_number_served(self,number):
+#         self.number_served = number
+#
+#     def get_number_served(self):
+#         return self.number_served
+#
+#     def increment_number_served(self,number):
+#         while self.number_served < number:
+#             print(self.number_served)
+#             self.number_served += 1
+#
+# restraurant = Restraurant('A','B')
+# print(restraurant.restaurant_name," ",restraurant.cuisine_type)
+# restraurant.describe_restaurant()
+# restraurant.open_restaurant()
+# print(restraurant.get_number_served())
+# restraurant.set_number_served(2)
+# print(restraurant.get_number_served())
+# restraurant.increment_number_served(10)
+
+
+# class User():
+#     """docstring for User"""
+#
+#     def __init__(self, first_name, last_name):
+#         self.first_name = first_name
+#         self.last_name = last_name
+#         self.login_attempts = 0
+#
+#     def describe_name(self):
+#         print("fitst_name :", self.first_name, " last_name: ", self.last_name)
+#
+#     def greet_user(self):
+#         print("hello, ", self.first_name, " ", self.last_name)
+#
+#     def increment_login_attempts(self):
+#         self.login_attempts += 1
+#
+#     def reset_login_attempts(self):
+#         self.login_attempts = 0
+#
+# user1 = User('Alice','Tom')
+# user1.describe_name()
+# print("login_attempts:",user1.login_attempts)
+# user1.increment_login_attempts()
+# print("login_attempts:",user1.login_attempts)
+# user1.increment_login_attempts()
+# print("login_attempts:",user1.login_attempts)
+# user1.reset_login_attempts()
+# print("login_attempts:",user1.login_attempts)
+
+
 class Restraurant():
-    def __init__(self,restraurant_name,cuisine_type):
-        self.restaurant_name = restraurant_name
+    def __init__(self,restaurant_name,cuisine_type):
+        self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
         self.number_served = 0
 
@@ -64,11 +128,16 @@ class Restraurant():
             print(self.number_served)
             self.number_served += 1
 
-restraurant = Restraurant('A','B')
-print(restraurant.restaurant_name," ",restraurant.cuisine_type)
-restraurant.describe_restaurant()
-restraurant.open_restaurant()
-print(restraurant.get_number_served())
-restraurant.set_number_served(2)
-print(restraurant.get_number_served())
-restraurant.increment_number_served(10)
+class IceCreamStand(Restraurant):
+    def __init__(self,restaurant_name,cuisine_type):
+        super().__init__(restaurant_name,cuisine_type)
+        self.flavors = ['banaa','apple','orange']
+    def show_icecram(self):
+        for s in self.flavors:
+            print(s)
+
+
+icecream = IceCreamStand('Hang','Big')
+icecream.show_icecram()
+icecream.describe_restaurant()
+
