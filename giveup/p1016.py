db3cd92e84658031ffc25b53961b0cad0aaab014
yuqihui <mg5851@126.com>
2018-10-17 10:30:29 +0800

    hh

--- giveup/p1016.py
+++ giveup/p1016.py
@@ -8,10 +8,15 @@ class Restaurent():
     def open_restaurant(self):
         print("it is opening")
 
-restaurent = Restaurent("A","B")
-print("---",restaurent.restaurent_name," ",restaurent.cuisine_type)
-restaurent.describe_restuarant()
-restaurent.open_restaurant()
+# restaurent = Restaurent("A","B")
+# print("---",restaurent.restaurent_name," ",restaurent.cuisine_type)
+# restaurent.describe_restuarant()
+# restaurent.open_restaurant()
 
-
+restraurant1 = Restaurent('A1','B1')
+restraurant1.describe_restuarant()
+restraurant2 = Restaurent('A2','B2')
+restraurant2.describe_restuarant()
+restraurant3 = Restaurent('A3','B3')
+restraurant3.describe_restuarant()
 
